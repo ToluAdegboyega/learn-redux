5e2977d722c44d7a3efe60d2bfc110c79f1c69bc
ToluAdegboyega <65776842+ToluAdegboyega@users.noreply.github.com>
2021-01-01 17:14:49 +0100

    chore: update learn-redux

---
+++ src/index.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { createStore } from "redux";
+import  allReducer from './reducers'
+import  { Provider }  from  'react-redux'
+
+const store = createStore(
+  allReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+
+/*
+
+//STORE -> GLOBALIZED STATE
+
+//ACTION INCREMENT
+const increment = () => {
+  return  {
+    type: 'INCREMENT'
+  }
+}
+
+const decrement = () => {
+  return  {
+    type: 'DECREMENT'
+  }
+}
+
+//REDUCER
+const counter = (state = 0, action) => {
+  switch  (action.type){
+    case  'INCREMENT':  
+     return  state + 1;
+    case 'DECREMENT':
+      return  state - 1;
+  }
+};
+
+let store = createStore(counter);
+
+//display in console
+store.subscribe(() => console.log(store.getState()));
+
+//DISPATCH
+store.dispatch(increment());
+store.dispatch(decrement());
+*/
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
